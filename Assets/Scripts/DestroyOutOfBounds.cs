b8d32ea8e735f37bc8cc8972a29a743869dbf1af
Yudong Li <a1923172548@gmail.com>
2025-03-26 12:29:54 -0700

    Added collision detect and game over log. Finished

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -20,8 +20,9 @@ public class DestroyOutOfBounds : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        if (transform.position.z < lowerBound)
+        else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game over");
             Destroy(gameObject);
         }
     }
