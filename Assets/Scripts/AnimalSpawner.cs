fd39fd78a0cb3c46ec7bff0876f50782952ac71d
Yudong Li <a1923172548@gmail.com>
2025-03-26 10:54:43 -0700

    Spawn animals on click

--- Assets/Scripts/AnimalSpawner.cs
+++ Assets/Scripts/AnimalSpawner.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class AnimalSpawner : MonoBehaviour
 {
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 10;
+    private float spwanRangeZ = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class AnimalSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spwanRangeZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
